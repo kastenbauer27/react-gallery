afa4be53fceeee1f2f668a3410c2b96d4f9141ef
kastenbauer27 <pkast2016@comcast.net>
2020-12-06 15:10:24 -0600

    set up conditional rendering function for image description

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -4,6 +4,7 @@ import './GalleryItem.css';
 class GalleryItem extends Component {
     state = { 
         photoLikes: 0,
+        showDescription: false
     }
 
     likePhoto = () => {
@@ -13,10 +14,21 @@ class GalleryItem extends Component {
         });
     }
 
+    toggleDescription = () => {
+        this.setState({
+            showDescription: !this.state.showDescription
+        });
+    }
+
     render() { 
         return ( 
             <div className="GalleryItem">
-                <img src={this.props.photo.path} />
+                {this.state.showDescription ?
+                    <div className="photoDescription" onClick={this.toggleDescription}>
+                        <p>{this.props.photo.description}</p>
+                    </div> :                
+                    <img src={this.props.photo.path} onClick={this.toggleDescription}/>
+                }
                 <button className="likeBtn" onClick={this.likePhoto}>Awesome Photo!</button>
                 <p>{this.state.photoLikes} people have liked this photo!</p>
             </div>
